f4a7a00d3bb212842ecce17385d97d2267ee402d
Dennis Hedback <d.hedback@gmail.com>
2022-07-19 16:49:58 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import os
+from setuptools import setup
+
+setup(
+    name="package",
+    version="0.1.0",
+    description="<one line to give the program's name and a brief idea of what it does.>",
+    long_description=open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+    ).read(),
+    long_description_content_type="text/markdown",
+    author="Dennis Hedback",
+    packages=["package"],
+    install_requires=[],
+)
